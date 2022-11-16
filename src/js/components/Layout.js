33f2ecb2b560c6018c26cdcb3e0623635c80618c
kzyymmt53 <kzyymmt53@gmail.com>
2022-11-16 20:59:51 +0900

    inputの入力時のevent処理

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -8,15 +8,13 @@ export default class Layout extends React.Component {
       super();
       this.state = {title: "Welcome"};
   }
+  changeTitle(title) {
+      this.setState({title});
+  }
   render() {
-    setTimeout(
-        () => { this.setState({title: "Welcome Test"}); },
-	2000
-    );
     return (
 	<div>
-	    <Header title={this.state.title} />
-	    <Header title={"Thank you"} />
+	    <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
             <Footer />
         </div>
     );
