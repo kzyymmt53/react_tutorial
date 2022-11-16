9047fd0045fc9cc77d16a477fad185d8b8479000
kzyymmt53 <kzyymmt53@gmail.com>
2022-11-16 20:24:57 +0900

    Header Footerコンポーネント

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
 
 export default class Layout extends React.Component {
   constructor() {
@@ -7,7 +10,12 @@ export default class Layout extends React.Component {
   }
   render() {
     return (
-      <h1>It's {this.name}!</h1>
+	<div>
+            <Header />
+            <Header />
+            <Header />
+            <Footer />
+        </div>
     );
   }
 }
