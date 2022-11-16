1539b769267c01841e119ab3087625401418878b
kzyymmt53 <kzyymmt53@gmail.com>
2022-11-16 20:20:22 +0900

    コンポーネント化

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Layout from "./components/Layout";
 
-class Layout extends React.Component {
-	render() {
-		return (
-			<h1>Welcome</h1>
-		);
-	}
-}
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
